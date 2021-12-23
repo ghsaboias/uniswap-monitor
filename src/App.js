ccc7f165d8e9c14df6fb037ecce5a7e33a5c11ad
Guilherme Saboia <gsaboia@yahoo.com>
2021-12-22 23:12:39 -0300

    update: adds ws client, not subscribing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import { createClient, useSubscription, defaultExchanges, subscriptionExchange } from 'urql'
+import { createClient as createWSClient } from 'graphql-ws';
 import { useEffect, useState } from 'react'
-import Swaps from './components/Swaps';
 import Header from './components/Header';
+import Swaps from './components/Swaps';
 
 const APIURL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3";
+
+const wsClient = createWSClient({
+  url: APIURL,
+});
+
+const client = createClient({
+  url: APIURL,
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({
+      forwardSubscription: (operation) => ({
+        subscribe: (sink) => ({
+          unsubscribe: wsClient.subscribe(operation, sink),
+        }),
+      }),
+    }),
+  ],
+});
+
 
 const query = `
   {
@@ -45,10 +65,9 @@ const query = `
     }
   `
 
-
-const client = createClient({
-  url: APIURL
-})
+// const client = createClient({
+//   url: APIURL
+// })
 
 function App() {
   const [swaps, setSwaps] = useState([]);
@@ -60,6 +79,9 @@ function App() {
   async function fetchData() {
     console.log('fetching');
     const response = await client.query(query).toPromise();
+    // throws errors, giving up for today
+    const response1 = await client.subscription(query1).toPromise();
+    console.log(response1);
     const { data } = response;
     const { swaps } = data;
     setSwaps(swaps);
